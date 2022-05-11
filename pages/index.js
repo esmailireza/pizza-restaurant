c4954cd3d707dbb7b1b9e7bf798c0b8aa6ca55ad
esmailireza <esmailireza20@gmail.com>
2022-05-11 12:24:07 +0430

    implement products list component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Featured from "../components/featured";
+import PizzaList from "../components/pizzaList";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Featured />
+      <PizzaList />
     </div>
   );
 }
